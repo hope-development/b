bfbe39063a8e1fbc1e0fb5b04b285c52cc9035cb
cubk <ceeyourback@outlook.com>
2025-05-20 21:03:13 +0800

    Update diy-part5.sh

--- diy-part5.sh
+++ diy-part5.sh
@@ -20,7 +20,7 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 # 移除USB网络共享
-sed -i 's/kmod-usb-net-rndis //g' target/linux/mediatek/image/mt7986.mk
+#sed -i 's/kmod-usb-net-rndis //g' target/linux/mediatek/image/mt7986.mk
 
 # 添加组播防火墙规则
 cat >> package/network/config/firewall/files/firewall.config <<EOF
